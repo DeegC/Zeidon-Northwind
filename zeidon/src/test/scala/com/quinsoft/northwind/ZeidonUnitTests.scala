05f88db7130805b359a42a1b44a2c785a6639a8c
deegc <deegc@gitweb.com>
2016-06-25 13:50:26 -0400

    Update to test to store OI as json

--- zeidon/src/test/scala/com/quinsoft/northwind/ZeidonUnitTests.scala
+++ zeidon/src/test/scala/com/quinsoft/northwind/ZeidonUnitTests.scala
@@ -35,6 +35,8 @@ class ZeidonUnitTests {
                 .to( _.Product.ProductName isNotNull )
             .readOnly
             .activate()
+
+        order.serializeOi.withIncremental.toFile("/tmp/orders.json")
     }
 
     @Test
