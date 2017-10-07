9f76121674e3417ad1a23582408d9db488d3843c
deegc <deegc@gitweb.com>
2017-10-07 17:23:59 -0400

    Add employee list

--- angular/src/app/order-detail.component.ts
+++ angular/src/app/order-detail.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input, Output, OnInit, EventEmitter, SimpleChanges, OnChanges } from '@angular/core';
 import { FormGroup, Validators } from '@angular/forms';
-import {ActivatedRoute} from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { NorthwindService } from './northwind.service';
 import { Customer} from './lod/Customer';
 import { Order } from './lod/Order';
+import { Employee } from './lod/Employee';
 import * as zeidon from './zeidon-angular';
 
 @Component({
@@ -16,6 +17,7 @@ export class OrderDetailComponent implements OnInit, OnChanges {
 
     order: Order;
     customerList: Customer;
+    employeeList: Employee;
     errorMessage: string;
     submitted = false;
     form: FormGroup;
@@ -60,6 +62,10 @@ export class OrderDetailComponent implements OnInit, OnChanges {
         Customer.activate( { rootOnly: true } ).subscribe( list => {
             this.customerList = list;
         } )
+
+        Employee.activate( { rootOnly: true } ).subscribe( list => {
+            this.employeeList = list;
+        } )
     }
 
     cancel(): void {
@@ -72,6 +78,15 @@ export class OrderDetailComponent implements OnInit, OnChanges {
             this.order.Order$.Customer$.exclude();
 
         this.order.Order$.Customer.include( customer );
+        this.buildForm();
+    }
+
+    onEmployeeSelected( employeeIdx ): void {
+        let employee = this.employeeList.Employee[ employeeIdx ];
+        if ( this.order.Order$.Employee$ )
+            this.order.Order$.Employee$.exclude();
+
+        this.order.Order$.Employee.include( employee );
         this.buildForm();
     }
 
