3fc79449b5df2d530a800d07ca3c89b7d0b12a1e
dgc <dgc@dgchristensen.net>
2020-12-02 19:35:35 -0500

    Update to use jaxrs Zeidon JAR file

--- scripts/activate.sh
+++ scripts/activate.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#curl -G --data-urlencode 'qual={"oi": {"id":10}}' localhost:4567/
 curl -G --data-urlencode 'qual={
   "OrderId": 10248
-}' $NORTHWIND_SERVER/northwind/Order
+}' http://localhost:8080/northwind-jaxrs-1.0/zeidon-api/Northwind/Order
